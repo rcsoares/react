e761dc46b1c80e3e1af579dab7487ef212981287
Rodrigo Soares <x02970863162@tcu.gov.br>
2019-05-03 11:07:34 -0300

    curso react avançado

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Welcome from './components/Welcome';
+import Signup from './components/auth/Signup';
+import { createStore } from 'redux';
+import reducers from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <Provider store={createStore(reducers)}>
+        <BrowserRouter>
+            <App>
+                <Route path="/" exact component={Welcome} />
+                <Route path="/signup" exact component={Signup} />
+            </App>
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root'));

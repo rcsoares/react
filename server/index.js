a3849179d1dce5d0065818103699654e3b68c903
Rodrigo Soares <x02970863162@tcu.gov.br>
2019-09-20 15:03:19 -0300

    servidor com autenticacao

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http')
+const bodyParser = require('body-parser')
+
+const cors = require('cors')
+
+const morgan = require('morgan')
+
+const app = express();
+
+const router = require('./router')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/auth', { useNewUrlParser: true })
+
+//App Setup
+app.use(morgan('combined'));
+
+app.use(cors())
+
+app.use(bodyParser.json({ type: '*/*' }))
+router(app)
+
+//Server Setup
+const port = process.env.port || 3090;
+
+const server = http.createServer(app);
+server.listen(port)
+console.log('Server listening on:', port)
